ca9f2bff2dd512527fb4010c9e573ff26cf20fd7
Vitalex <vitalex951@gmail.com>
2024-07-26 16:03:05 +0300

    added sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { useEffect } from "react";
+import { useAppDispatch } from "./store/store.ts";
+import { fetchTodos } from "./store/service/fetchTodos.ts";
 
 function App() {
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(fetchTodos())
+    }, [])
 
     return (
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
+      <main className={ 'm-0' }>
+          <h1 className='font-bold size-21 caret-emerald-400 m-0'>
+              Todos
+          </h1>
+      </main>
     )
 }
 
