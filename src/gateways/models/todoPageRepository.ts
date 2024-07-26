3f54b616e897daf240f136ed21e24238a83fa9ef
Vitalex <vitalex951@gmail.com>
2024-07-26 13:34:58 +0300

    added mock server and data

---
+++ src/gateways/models/todoPageRepository.ts
@@ -0,0 +1,23 @@
+import { ITodo } from "./todo.ts";
+
+/**
+ * Интерфейс репозитория для получения данных для страницы задач
+ */
+export interface ITodoPageRepository {
+    /**
+     * Возвращает всех задач
+     */
+    getTodos(): Promise<ITodo[]>;
+    /**
+     * Удаление задачи по id
+     */
+    deleteTodo(todoId: number): Promise<ITodo[]>;
+    /**
+     * Редактирование задачи по id
+     */
+    updateTodo(todo: ITodo): Promise<ITodo>;
+    /**
+     * Создание новой задачи
+     */
+    createTodo(todo: ITodo): Promise<ITodo>;
+}
